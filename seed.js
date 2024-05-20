b49b94ccd74aa952d3454112e7625bb37b166ca4
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-20 16:48:52 +0100

    seeded other models

--- seed.js
+++ seed.js
@@ -1,13 +1,12 @@
-const {Restaurant} = require("./models/index")
-const { seedRestaurant } = require("./seedData");
+const { Restaurant,Menu,Item } = require("./models/index")
+const { seedRestaurant, seedItem, seedMenu } = require("./seedData");
 const db = require("./db/connection")
 
 const syncSeed = async () => {
     await db.sync({force: true});
     await Restaurant.bulkCreate(seedRestaurant)
-    // BONUS: Update with Item and Menu bulkCreate
-
-
+    await Menu.bulkCreate(seedMenu)
+    await Item.bulkCreate(seedItem)
 }
 
 syncSeed()
