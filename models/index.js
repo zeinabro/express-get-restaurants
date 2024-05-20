bdfc69baf5549b0d3717f07676e2f6ddcadea681
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-20 16:45:21 +0100

    model associations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,12 @@
 const Restaurant = require('./Restaurant')
 const Model = require("./Menu");
 const Item = require('./Item');
+const Menu = require('./Menu');
+
+Restaurant.hasMany(Menu)
+Menu.belongsTo(Restaurant)
+
+Menu.belongsToMany(Item)
+Item.belongsToMany(Menu)
 
 module.exports = {Restaurant,Model,Item};
