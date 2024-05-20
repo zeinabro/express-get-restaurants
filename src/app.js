e70722879f96718e8433740d3aaad26c3c2487fa
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-20 10:28:23 +0100

    get all restaurants and restaurant by id, auto update server

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async(req,res)=>{
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants)
+})
 
-
-
+app.get("/restaurants/:id", async(req,res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    res.json(restaurant)
+})
 
 module.exports = app;
